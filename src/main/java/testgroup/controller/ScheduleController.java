dada83e638521109880c1219b14e1db057b7c465
admin <Serdyk87@ukr.net>
2019-07-12 10:57:45 +0300

    fgf

--- src/main/java/testgroup/controller/ScheduleController.java
+++ src/main/java/testgroup/controller/ScheduleController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import testgroup.model.Lesson;
 import testgroup.service.LessonService;
-import testgroup.service.Parser;
 
 import java.util.Collections;
 import java.util.List;
